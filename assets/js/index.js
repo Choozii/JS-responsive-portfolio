c1c25b5d47d0111b608abffaba72a16f80e243c6
Choozii <jiyechoo@gmail.com>
2021-04-06 21:57:38 +0900

    [feat] media query 적용

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+/*=== nav ===*/
 const close = document.getElementById('nav-close'),
 navMenu = document.getElementById('nav-menu'),
 open = document.getElementById('nav-open');
@@ -8,3 +9,19 @@ close.addEventListener('click', ()=>{
 open.addEventListener('click', ()=>{
     navMenu.classList.toggle('show');
 })
+
+const contactButton = document.getElementById('contactBtn');
+contactButton.addEventListener('click', ()=>{
+   let mailto = `mailto:jiyechoo@gmail.com?subject=${""}&body=${""}`;
+   window.open(mailto, 'emailWindow');
+})
+
+/*=== animation effect ===*/
+
+gsap.from('.nav__open', {opacity : 0, duration : 1, delay:2, y:10})
+gsap.from('.nav__item', {opacity : 0, duration : 1, delay : 2.1, y: 30, stagger : 0.2})
+
+gsap.from('.home__title', {opacity : 0, duration : 1, delay : 1.6, y: 30})
+gsap.from('.home__description', {opacity : 0, duration : 1, delay : 1.8, y: 30})
+gsap.from('.home__button', {opacity : 0, duration : 1, delay : 2.1, y: 30})
+gsap.from('.home__image', {opacity : 0, duration : 1, delay : 1.3, y: 30})
